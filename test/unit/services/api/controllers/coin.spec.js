28a2c34cb77428321992f1f3763aaaa77bf34bf7
marcoref3at <marco_refaat@hotmail.com>
2022-04-02 20:04:29 +0200

    getCoin Test

--- test/unit/services/api/controllers/coin.spec.js
+++ test/unit/services/api/controllers/coin.spec.js
@@ -4,7 +4,7 @@ const sequelizeMockingMocha = require('sequelize-mocking').sequelizeMockingMocha
 const CoinController = require(path.join(srcDir, '/services/api/controllers/coin'));
 const DB = require(path.join(srcDir, 'modules/db'));
 
-describe('Controller: Coin', () => {
+describe.only('Controller: Coin', () => {
   let sandbox = null;
 
   sequelizeMockingMocha(DB.sequelize, [path.resolve('test/mocks/coins.json')], { logging: false });
@@ -19,16 +19,22 @@ describe('Controller: Coin', () => {
 
   describe('getCoinByCode', () => {
     it('should get coin by code', async () => {
-      const coinCode = 'BTC';
+      const coinCode = '2x2';
       const coin = await CoinController.getCoinByCode(coinCode);
 
       expect(coin.code).to.eq(coinCode);
-      expect(Object.keys(coin).length).to.eq(3);
+      expect(coin.price).to.be.a('number');
+      expect(Object.keys(coin).length).to.eq(4);
     });
 
     it('should fail get coin by code', async () => {
       const coinCode = 'AMN';
       expect(CoinController.getCoinByCode(coinCode)).to.be.rejectedWith(Error, 'unknown_coin_code');
+    });
+
+    it('should fail get coin price', async () => {
+      const coinCode = 'BTC';
+      expect(CoinController.getCoinByCode(coinCode)).to.be.rejectedWith(Error, 'coingecko_API_error');
     });
   });
 });
